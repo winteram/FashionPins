e46d70ceebfa123a7db236d6a00eef5ac1586783
ps <pozturk@stevens.edu>
2013-02-11 12:16:16 -0500

    Some more updates

--- lastCrawl.py
+++ lastCrawl.py
@@ -13,43 +13,73 @@ trackar=[]
 fans=[]
 
 fanlib={}
+inartists=['Etta James']
 
-inartists=['Billie Holiday','Charles Aznavour']
-# get initial top 100 artists and their top 2 tracks and topfans of those 2 tracks
-for y in range(0,len(inartists)):
-    artist = network.get_artist(inartists[y])
-    top_tracks=artist.get_top_tracks()
-    trackar=[]
-    for i in range(0,2):
-        for top_track in top_tracks:
-            trackar.append(top_track.item.get_name())
-            tkey=str(artist)+'-'+trackar[i]
-            if tkey not in tracklib:
-                tracklib[tkey]=1
+# get initial top 100 artists and their top 2 tracks
+def getinitial(inartists):
+    for y in range(0,len(inartists)):
+        artist = network.get_artist(inartists[y])
+        top_tracks=artist.get_top_tracks()
+        trackar=[]
+        for i in range(0,2):
+            for top_track in top_tracks:
+                trackar.append(top_track.item.get_name())
+                tkey=str(artist)+'-'+trackar[i]
+                if tkey not in tracklib:
+                    tracklib[tkey]=1
+    print "Number of initial tracks: " +str(len(tracklib))
+    print tracklib
+    return tracklib
+
+# get topfans of tracks
+def topfans(tracklib):
+    fans=[]
+    for item in tracklib:
+        lartist=item.split('-')[0]
+        ltrack=item.split('-')[1]
+        # Got an error of "Track not found" so thought this might work
+        try:
+            track=network.get_track(lartist,ltrack)
+        except IOError, e:
+            if hasattr(e, 'reason'):
+                print 'Something.'
+                print 'Reason: ', e.reason
+            elif hasattr(e, 'code'):
+                print 'Something else.'
+                print 'Error code: ', e.code
+        else:
+            # Change the number of top fans here - if limit=None, returns 50
+            topfans=track.get_top_fans(limit=3)
+            for topfan in topfans:
+                name=topfan.item.get_name()
+                if name not in fans:
+                    fans.append(name)
+        print "Number of fans: " +str(len(fans))
+        print fans
+        return fans
+
+#get top tracks of fans - returns top 50 tracks
+def toptracks(fans):
+    for a in range(0,len(fans)):
+        fan=network.get_user(fans[a])
+        topfantracks=fan.get_top_tracks()
+        for topfantrack in topfantracks:
+            track=topfantrack.item.get_name()
+            track_name=track.encode('utf-8')
+            artist=topfantrack.item.get_artist().get_name()
+            artist_name=artist.encode('utf-8')
+            key=unicode(str(artist_name),'utf-8')+'-'+unicode(str(track_name),'utf-8')+'-'+fans[a]
+            if key not in fanlib:
+                fanlib[key]=1
+    print "Number of toptracks: " +str(len(fanlib))
+    return fanlib
 
 
-for item in tracklib:
-    lartist=item.split('-')[0]
-    ltrack=item.split('-')[1]
-    track=network.get_track(lartist,ltrack)
-    topfans=track.get_top_fans(limit=1)
-    for topfan in topfans:
-        name=topfan.item.get_name()
-        if name not in fans:
-            fans.append(name)
-        
-
-#get top tracks of fans
-for a in range(0,len(fans)):
-    fan=network.get_user(fans[a])
-    topfantracks=fan.get_top_tracks()
-    for topfantrack in topfantracks:
-        track=topfantrack.item.get_name()
-        artist=topfantrack.item.get_artist().get_name()
-# I am running to a unicode problem here when trying to concatenate the track name and user name; I've tried unicode and .decode('utf-8')
-        key=str(track)+'-'+str(track)+'-'+fans[a]
-        print key
-        if key not in fanlib:
-            fanlib[key]=1
-
+if __name__=="__main__":
+    tracklib=getinitial(inartists)
+    fans=topfans(tracklib)
+    for z in range(0,3):     
+        fanlib=toptracks(fans)
+        fans=topfans(fanlib)
+    
 
