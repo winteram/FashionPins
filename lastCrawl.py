b631057a7c1c3ae52283d7cfb15156238feff612
ps <pozturk@stevens.edu>
2013-02-12 11:00:25 -0500

    import from csv added
    Error for "Track not found"

--- lastCrawl.py
+++ lastCrawl.py
@@ -1,4 +1,5 @@
 import pylast
+import csv
 
 API_KEY = '8b2fa4cb683e168f66f47adcc708ad22'
 API_SECRET = '96f5ba11b4313fca6a34b65bba5c5843'
@@ -11,11 +12,21 @@ network = pylast.LastFMNetwork(api_key = API_KEY, api_secret =
 tracklib={}
 trackar=[]
 fans=[]
+tracks=[]
 
 fanlib={}
-inartists=['Etta James']
+
 
 # get initial top 100 artists and their top 2 tracks
+def getartist():
+    artistlist=artistlist = csv.reader(open("TopArtists.csv", "rb"))
+    inartists = []
+    for data in artistlist:
+        inartists.append(data[0])
+    #print inartists
+    return inartists
+
+# get initial artists top 2 songs and the fans
 def getinitial(inartists):
     for y in range(0,len(inartists)):
         artist = network.get_artist(inartists[y])
@@ -27,39 +38,52 @@ def getinitial(inartists):
                 tkey=str(artist)+'-'+trackar[i]
                 if tkey not in tracklib:
                     tracklib[tkey]=1
+        for item in tracklib:
+            lartist=item.split('-')[0]
+            ltrack=item.split('-')[1]  
+            if type(network.get_track(lartist,ltrack)) == None:
+                print "Track Not Found"          
+            else:
+                track=network.get_track(lartist,ltrack)
+                # Change the number of top fans here - if limit=None, returns 50
+                topfans=track.get_top_fans(limit=1)
+                for topfan in topfans:
+                    name=topfan.item.get_name()
+                    if name not in fans:
+                        fans.append(name)
     print "Number of initial tracks: " +str(len(tracklib))
-    print tracklib
-    return tracklib
+    print "Number of fans of the initial tracks: " +str(len(fans))
+    #print tracklib
+    return fans
 
 # get topfans of tracks
-def topfans(tracklib):
+def topfans(tracks):
     fans=[]
-    for item in tracklib:
-        lartist=item.split('-')[0]
-        ltrack=item.split('-')[1]
-        # Got an error of "Track not found" so thought this might work
-        try:
-            track=network.get_track(lartist,ltrack)
-        except IOError, e:
-            if hasattr(e, 'reason'):
-                print 'Something.'
-                print 'Reason: ', e.reason
-            elif hasattr(e, 'code'):
-                print 'Something else.'
-                print 'Error code: ', e.code
+    for b in range(len(tracks)):
+        lartist=tracks[b].split('-')[0]
+        ltrack=tracks[b].split('-')[1]  
+        
+        # Got an error of "Track not found" so thought this might work, it doesn't.
+        if type(network.get_track(lartist,ltrack)) == None:
+            print "Track Not Found"     
+            
         else:
+            track=network.get_track(lartist,ltrack)
+            #print track
             # Change the number of top fans here - if limit=None, returns 50
-            topfans=track.get_top_fans(limit=3)
+            topfans=track.get_top_fans(limit=1)
             for topfan in topfans:
                 name=topfan.item.get_name()
                 if name not in fans:
                     fans.append(name)
-        print "Number of fans: " +str(len(fans))
-        print fans
-        return fans
+                
+    print "Number of fans: " +str(len(fans))
+    #print fans
+    return fans
 
 #get top tracks of fans - returns top 50 tracks
 def toptracks(fans):
+    tracks=[]
     for a in range(0,len(fans)):
         fan=network.get_user(fans[a])
         topfantracks=fan.get_top_tracks()
@@ -68,18 +92,21 @@ def toptracks(fans):
             track_name=track.encode('utf-8')
             artist=topfantrack.item.get_artist().get_name()
             artist_name=artist.encode('utf-8')
+            tracks.append(str(artist_name)+'-'+str(track_name))
             key=unicode(str(artist_name),'utf-8')+'-'+unicode(str(track_name),'utf-8')+'-'+fans[a]
             if key not in fanlib:
                 fanlib[key]=1
-    print "Number of toptracks: " +str(len(fanlib))
-    return fanlib
+    print "Number of new tracks: " +str(len(tracks))
+    print "Number of fanlib: " +str(len(fanlib))
+    return tracks,fanlib
 
 
 if __name__=="__main__":
+    #inartists=getartist()
+    inartists=['Maroon 5']
     tracklib=getinitial(inartists)
-    fans=topfans(tracklib)
-    for z in range(0,3):     
-        fanlib=toptracks(fans)
-        fans=topfans(fanlib)
+    for z in range(0,2):     
+        tracks,fanlib=toptracks(fans)
+        fans=topfans(tracks)
     
 
